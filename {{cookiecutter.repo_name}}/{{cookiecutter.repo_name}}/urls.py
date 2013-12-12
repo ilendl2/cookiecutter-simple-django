b1882e5536ebbf5e6b04b055e5cca283dc307fab
Lendl Smith <lendl.smith@gmail.com>
2013-12-12 14:04:27 -0400

    changes to templates and addition of homepage

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
@@ -4,13 +4,19 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+class HomePageView(TemplateView):
+    template_name = "homepage.html"
+
 urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
+    url(r"^$",
+        HomePageView.as_view(),
+        name="home"),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
